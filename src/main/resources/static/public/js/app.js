637164c82addcd951f9225ca164afa6de6dc1591
Michal Marciniak <michal.marciniak@allegrogroup.com>
2017-03-13 22:57:04 +0100

    wip

--- src/main/resources/static/public/js/app.js
+++ src/main/resources/static/public/js/app.js
@@ -1,32 +1,64 @@
-(function() {
-    var button = document.getElementById('add');
+(function () {
+    var app = document.getElementById('app');
+    var form;
 
-    var HEADERS = {
-        'Content-Type': 'application/json'
+    var createElement = function (tag, attributes) {
+        var element = document.createElement(tag);
+
+        for (var key in attributes) {
+            if (attributes.hasOwnProperty(key)) {
+                element[key] = attributes[key];
+            }
+        }
+
+        return element;
     };
 
-    var bill = {
-        name: 'test bill',
-        positions: [{
-            name: 'piwo',
-            price: 20,
-            persons: [{
-                name: 'Jarek'
-            }, {
-                name: 'Michał'
-            }]
-        }]
+    var appendChildren = function (parent, children) {
+        children.forEach(function (child) {
+            parent.appendChild(child);
+        });
     };
 
-    button.onclick = function () {
+    var submitForm = function (ev) {
+        ev.preventDefault();
+        var data = new FormData(form);
+        var json = {
+            name: data.get('name')
+        };
+
         fetch('/bills/', {
                 method: 'POST',
-                body: JSON.stringify(bill),
-                headers: HEADERS
+                body: JSON.stringify(json),
+                headers: {
+                    'contentType': 'application/json'
+                }
             })
-            .then(function (response) {
-                window.resp = response;
-            })
-            .catch(console.error)
+            .then(console.log)
+            .catch(console.error);
     };
+
+    var generateForm = function () {
+        form = createElement('form');
+        var input = createElement('input', {
+            type: 'text',
+            name: 'name',
+            id: 'name'
+        });
+        var label = createElement('label', {
+            htmlFor: 'name',
+            innerHTML: 'Nazwa rachunku'
+        });
+        var button = createElement('button', {
+            type: 'submit',
+            innerHTML: 'Zapisz'
+        });
+
+        button.onclick = submitForm;
+
+        appendChildren(form, [input, label, button]);
+        app.appendChild(form);
+    };
+
+    generateForm();
 })();
