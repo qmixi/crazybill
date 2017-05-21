bf0be3c235b6de922b83b4803b3c70bdf5d9e81f
Jaroslaw Dlugosz <allegro.tech.umk@gmail.com>
2017-05-22 01:27:20 +0200

    notification by email

--- src/test/java/pl/allegro/umk/crazybill/config/MailTestConfig.java
+++ src/test/java/pl/allegro/umk/crazybill/config/MailTestConfig.java
@@ -14,24 +14,6 @@ import java.util.Properties;
 @Profile("test")
 @Configuration
 public class MailTestConfig {
-    @Value("${mail.protocol}")
-    private String protocol;
-    @Value("${mail.host}")
-    private String host;
-    @Value("${mail.port}")
-    private int port;
-    @Value("${mail.smtp.socketFactory.port}")
-    private int socketPort;
-    @Value("${mail.smtp.auth}")
-    private boolean auth;
-    @Value("${mail.smtp.starttls.enable}")
-    private boolean starttls;
-    @Value("${mail.smtp.starttls.required}")
-    private boolean startlls_required;
-    @Value("${mail.smtp.debug}")
-    private boolean debug;
-    @Value("${mail.smtp.socketFactory.fallback}")
-    private boolean fallback;
     @Value("${mail.from}")
     private String from;
 
